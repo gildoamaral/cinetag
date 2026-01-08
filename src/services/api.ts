819761d1d0828dd1834ed0f77813871c12140dd5
Marco Antonio Gil <gildo_marco@hotmail.com>
2026-01-08 15:39:20 -0300

    refactor: integrando com API TMDb

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,20 +1,35 @@
 import type { Video } from '@/types'
 
+const API_KEY = import.meta.env.VITE_API_KEY;
 const API_BASE = import.meta.env.VITE_API_BASE;
+const IMG_BASE = import.meta.env.VITE_IMG_BASE;
+
+const mapToVideo = (movie: any): Video => ({
+  id: movie.id.toString(),
+  titulo: movie.title,
+  capa: `${IMG_BASE}${movie.poster_path}`,
+  link: movie.videos?.results.find((v: any) => v.type === 'Trailer') 
+        ? `https://www.youtube.com/embed/${movie.videos.results.find((v: any) => v.type === 'Trailer').key}`
+        : `https://www.youtube.com/embed/dQw4w9WgXcQ` 
+});
 
 export async function fetchVideos(): Promise<Video[]> {
-  const response = await fetch(`${API_BASE}/videos`)
+  const response = await fetch(`${API_BASE}/movie/popular?api_key=${API_KEY}&language=pt-BR&append_to_response=videos`)
   if (!response.ok) {
     throw new Error('Erro ao carregar vídeos')
   }
-  return response.json()
+  const data = await response.json()
+  console.log(data.results.map(mapToVideo));
+  return data.results.map(mapToVideo)
 }
 
 export async function fetchVideoById(id: string): Promise<Video | null> {
-  const response = await fetch(`${API_BASE}/videos?id=${id}`)
+  const response = await fetch(`${API_BASE}/movie/${id}?api_key=${API_KEY}&language=pt-BR&append_to_response=videos`)
+  
   if (!response.ok) {
-    throw new Error('Erro ao carregar vídeo')
+    return null
   }
-  const data: Video[] = await response.json()
-  return data.length > 0 ? data[0] : null
+  
+  const data = await response.json()
+  return mapToVideo(data)
 }
